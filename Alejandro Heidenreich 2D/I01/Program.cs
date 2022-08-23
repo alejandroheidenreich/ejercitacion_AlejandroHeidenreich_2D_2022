fcc8d77c5545f1813c5be770ca716632d65b86fe
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2022-08-22 22:33:32 -0300

    clase1

--- Alejandro Heidenreich 2D/I01/Program.cs
+++ Alejandro Heidenreich 2D/I01/Program.cs
@@ -9,11 +9,14 @@ namespace I01
     {
         static void Main(string[] args)
         {
-            int numero;
+            int numeroIngresado;
             int maximo = 6;
             int minimo = 9;
+            int promedioNumeros;
+            int contadorDeNoNumeros = 0;
+            int acumuladorNumeros = 0;
             string numeroStr;
-            bool flag = true;
+            bool banderaPrimeroNumero = true;
 
 
             for (int i = 0; i < 5; i++)
@@ -22,40 +25,43 @@ namespace I01
 
                 numeroStr = Console.ReadLine();
 
-                if (int.TryParse(numeroStr, out numero))
+                if (int.TryParse(numeroStr, out numeroIngresado))
                 {
-                    if (flag)
+                    acumuladorNumeros += numeroIngresado;
+                    if (banderaPrimeroNumero)
                     {
-                        maximo = numero;
-                        minimo = numero;
-                        flag = false;
+                        maximo = numeroIngresado;
+                        minimo = numeroIngresado;
+                        banderaPrimeroNumero = false;
                     }
                     else
                     {
-                        if (numero > maximo)
+                        if (numeroIngresado > maximo)
                         {
-                            maximo = numero;
+                            maximo = numeroIngresado;
                         }
-                        if (numero < minimo)
+                        if (numeroIngresado < minimo)
                         {
-                            minimo = numero;
+                            minimo = numeroIngresado;
                         }
                     }
                 }
                 else
                 {
                     Console.Write($"El dato ingresado en el puesto {i + 1} no es un numero");
+                    contadorDeNoNumeros++;
                 }
 
             }
 
-            if (flag)
+            if (banderaPrimeroNumero)
             {
                 Console.Write($"No se ingresaron numeros");
             }
             else
             {
-                Console.Write($"El numero mayor es {maximo} {Environment.NewLine} El numero menor es {minimo}");
+                promedioNumeros = acumuladorNumeros / (5 - contadorDeNoNumeros);
+                Console.Write($"El numero mayor es {maximo} {Environment.NewLine}El numero menor es {minimo}{Environment.NewLine}El promedio de los numeros ingreados es {promedioNumeros}");
             }
         }
     }
---
+++ Alejandro Heidenreich 2D/I02/Program.cs
@@ -0,0 +1,15 @@
+﻿//Ingresar un número y mostrar el cuadrado y el cubo del mismo. Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!". (Utilizar el método Pow de la clase Math para calcular las potencias.)
+using System;
+
+namespace I02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+        }
+    }
+}
