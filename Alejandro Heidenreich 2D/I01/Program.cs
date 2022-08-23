5e4c2aa1d3e95a432a6cb183c92498f2e55826ed
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2022-08-22 21:25:01 -0300

    ejer01

---
+++ Alejandro Heidenreich 2D/I01/Program.cs
@@ -0,0 +1,55 @@
+﻿//Ingresar 5 números por consola, guardándolos en una variable escalar.
+//Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+
+using System;
+
+namespace I01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int maximo;
+            int minimo;
+            string numeroStr;
+            bool flag = true;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un numero");
+
+                numeroStr = Console.ReadLine();
+
+                if (int.TryParse(numeroStr, out numero))
+                {
+                    if (flag)
+                    {
+                        maximo = numero;
+                        minimo = numero;
+                        flag = false;
+                    }
+                    else
+                    {
+                        if (numero > maximo)
+                        {
+                            maximo = numero;
+                        }
+                        if (numero < minimo)
+                        {
+                            minimo = numero;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.Write($"El dato ingresado en el puesto {i + 1} no es un numero");
+                }
+
+
+            }
+
+            Console.Write($"El numero mayor es {maximo} {Environment.NewLine} El numero menor es {minimo}");
+        }
+    }
+}
