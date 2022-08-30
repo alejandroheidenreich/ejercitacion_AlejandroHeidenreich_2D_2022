a4ab5cb7fd3ef94cf39eb8d0838a826629f94181
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2022-08-30 12:31:07 -0300

    Cuantas Primaveras Ejercicio I02

--- Alejandro Heidenreich 2D/Cuantas Primaveras Ejercicio I02/Persona.cs
+++ Alejandro Heidenreich 2D/Cuantas Primaveras Ejercicio I02/Persona.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,75 @@ namespace Cuantas_Primaveras_Ejercicio_I02
     {
         public string nombre;
         public DateTime fechaDeNacimiento;
+        public string dni;
 
-        public Persona()
+        public Persona(string nombre,int dia, int mes, int año,string dni)
         {
-
+            this.nombre = nombre;
+            SetNacimiento(dia, mes, año);
+            this.dni = dni; 
         }
+
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+
+        public string GetNacimiento()
+        {
+            return this.fechaDeNacimiento.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture); ;
+        }
+
+        public string GetDNI()
+        {
+            return this.dni;
+        }
+
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void SetNacimiento(int dia, int mes, int año)
+        {
+            DateTime fecha = new DateTime(año, mes, dia);
+
+            this.fechaDeNacimiento = fecha;
+        }
+
+        public void SetDNI(string dni)
+        {
+            this.dni = dni;
+        }
+
+        public int CalcularEdad()
+        {
+            int edad;
+            edad = DateTime.Now.Year - this.fechaDeNacimiento.Year;
+            if (this.fechaDeNacimiento.Date > DateTime.Now.AddYears(-edad))
+            {
+                edad--;
+            }
+
+            return edad;
+        }
+
+        public string MostrarPersona()
+        {
+            return $"{GetNombre()}\n{GetNacimiento()}\n{GetDNI()}";
+        }
+
+        public string EsmayorDeEdad()
+        {
+            string respuesta = "Es mayor de edad";
+            if (this.CalcularEdad() < 17)
+            {
+               respuesta = "Es menor";
+            }
+
+            return respuesta;
+        }
+
     }
 
    
--- Alejandro Heidenreich 2D/Cuantas Primaveras Ejercicio I02/Program.cs
+++ Alejandro Heidenreich 2D/Cuantas Primaveras Ejercicio I02/Program.cs
@@ -6,7 +6,22 @@ namespace Cuantas_Primaveras_Ejercicio_I02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona personaUno = new Persona("Roberto",10,10,1989,"11598751");
+            Persona personaDos = new Persona("Raul",28,08,1989,"10107833");
+            Persona personaTres = new Persona("Raul",06,03,2010,"52366012");
+
+            Console.WriteLine(personaUno.CalcularEdad());
+            Console.WriteLine(personaDos.CalcularEdad());
+            Console.WriteLine(personaTres.CalcularEdad());
+
+            Console.WriteLine(personaUno.MostrarPersona());
+            Console.WriteLine(personaUno.EsmayorDeEdad());
+
+            Console.WriteLine(personaDos.MostrarPersona());
+            Console.WriteLine(personaDos.EsmayorDeEdad());
+
+            Console.WriteLine(personaTres.MostrarPersona());
+            Console.WriteLine(personaTres.EsmayorDeEdad());
         }
     }
 }
