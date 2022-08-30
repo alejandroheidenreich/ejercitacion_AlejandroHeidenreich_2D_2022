a44646fe2d8cd4a8fcba39b6dcf3b1dde7214ae4
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2022-08-30 10:54:54 -0300

    ejercicio factorial

---
+++ Alejandro Heidenreich 2D/Clase 03 - Clases y metodos estaticos/Biblioteca/Matematica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Matematica
+    {
+        public static int CalcularFactorial (int numeroIngresado)
+        {
+            if(numeroIngresado == 0 || numeroIngresado == 1)
+            {
+                return 1;
+            }
+            return numeroIngresado * CalcularFactorial(numeroIngresado-1);
+        }
+    }
+}
---
+++ Alejandro Heidenreich 2D/Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using Biblioteca;
+using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            int factorialDelNumero;
+            Entrada.PedirEntero("Ingrese un numero entero","ERROR - No es un numero entero", out numeroIngresado);
+
+            factorialDelNumero = Matematica.CalcularFactorial(numeroIngresado);
+
+            Console.WriteLine($"El factorial de {numeroIngresado} es: {factorialDelNumero}");
+        }
+    }
+}
