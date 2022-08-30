06aa3292cde311220d69aefc9a91863fada5252e
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2022-08-30 14:54:59 -0300

    ejercicio I04

---
+++ Alejandro Heidenreich 2D/Clase 02 - Programacion Objetos/Ejercicio I04/Program.cs
@@ -0,0 +1,37 @@
+﻿using Entidades;
+using System;
+
+namespace Ejercicio_I04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Boligrafo boligrafoUno = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo boligrafoDos = new Boligrafo(ConsoleColor.Red, 50);
+            string dibujo;
+
+            Console.WriteLine($"Color del primer boligrafo: {boligrafoUno.GetColor()}");
+            Console.WriteLine($"Nivel de tinta del primer boligrafo: {boligrafoUno.GetTinta()}");
+            Console.WriteLine($"Color del segundo boligrafo: {boligrafoDos.GetColor()}");
+            Console.WriteLine($"Nivel de tinta del segundo boligrafo: {boligrafoDos.GetTinta()}");
+
+            boligrafoDos.Recargar();
+            Console.WriteLine($"Nivel de tinta del segundo boligrafo: {boligrafoDos.GetTinta()}");
+
+            boligrafoUno.Pintar(10, out dibujo);
+            Console.WriteLine($"Nivel de tinta del primer boligrafo: {boligrafoUno.GetTinta()}");
+            Console.ForegroundColor = boligrafoUno.GetColor();
+            Console.WriteLine(dibujo);
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            boligrafoDos.Pintar(25, out dibujo);
+            Console.WriteLine($"Nivel de tinta del primer boligrafo: {boligrafoDos.GetTinta()}");
+            Console.ForegroundColor = boligrafoDos.GetColor();
+            Console.WriteLine(dibujo);
+
+        }
+    }
+}
---
+++ Alejandro Heidenreich 2D/Clase 02 - Programacion Objetos/Entidades/Boligrafo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Boligrafo
+    {
+        
+        const short CANTIDADTINTAMAXIMA = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+        
+        private void SetTinta(short tinta)
+        {
+            if (tinta <= CANTIDADTINTAMAXIMA)
+            {
+                this.tinta = tinta;
+            }
+        }
+        public void Recargar()
+        {
+            SetTinta(CANTIDADTINTAMAXIMA);
+        }
+        public bool Pintar(short gasto, out string dibujo)
+        {
+            int restoTinta;
+            dibujo = "";
+
+            if (gasto < GetTinta())
+            {
+                restoTinta = (int)GetTinta() - gasto;
+
+                this.SetTinta((short)restoTinta);
+                for (int i = 0; i < gasto; i++)
+                {
+                     dibujo += "*";
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
