3bc8b1db1c8885ad2187f4a81d940dbb90b77b5c
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2022-08-30 11:33:33 -0300

    Ejercicio Prestamo

---
+++ Alejandro Heidenreich 2D/Prestamo Ejercicio I01/Cuenta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestamo_Ejercicio_I01
+{
+    public class Cuenta
+    {
+        string titular;
+        double cantidad;
+
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string GetTitular()
+        {
+            return this.titular;
+        }
+
+        public double GetCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public string MostrarCuenta()
+        {
+            return $"Cuenta\nTitular: {GetTitular()}\nCantidad: ${GetCantidad().ToString("N2")}";
+        }
+        
+        public void IngresarMontoACuenta(double cantidadIngresada)
+        {
+            if (cantidadIngresada > 0)
+            {
+                this.cantidad+=cantidadIngresada;
+            }
+        }
+
+        public void RetirarMontoDeCuenta(double cantidadIngresada)
+        {
+            if (cantidadIngresada > 0)
+            {
+                this.cantidad -= cantidadIngresada;
+            }
+        }
+    }
+}
---
+++ Alejandro Heidenreich 2D/Prestamo Ejercicio I01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Prestamo_Ejercicio_I01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta primerCuenta = new Cuenta("Roberto S.A.", 1000);
+
+            Console.WriteLine(primerCuenta.GetTitular());
+
+            primerCuenta.IngresarMontoACuenta(500.5555);
+
+            Console.WriteLine(primerCuenta.GetCantidad().ToString("N2"));
+
+            primerCuenta.RetirarMontoDeCuenta(250);
+
+            Console.WriteLine(primerCuenta.GetCantidad().ToString("N2"));
+
+            Console.WriteLine(primerCuenta.MostrarCuenta());
+        }
+    }
+}
